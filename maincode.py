a19c5570b1c03ff2cb0b5eac090065ed5c65c82b
Angel Rivera <38502560+anmeth191@users.noreply.github.com>
2023-12-17 22:20:43 -0500

    Update maincode.py

--- maincode.py
+++ maincode.py
@@ -21,10 +21,10 @@ class PredefinedCharts(CanvasDims):
            self.sidenav = sidenav
             #Navbar dims by default
            if self.navbar.lower() == 'y':
-               self.navbar_height = 90
-               self.navbar_width = self.width
-               self.navbar_horizontal = 0
-               self.navbar_vertical = 0
+               self.navbar_height = 90 
+               self.navbar_width = self.width - (self.horizontal * 2)
+               self.navbar_horizontal = self.horizontal
+               self.navbar_vertical = self.vertical
            else:
                self.navbar_height = 0
                self.navbar_width = 0
@@ -34,9 +34,9 @@ class PredefinedCharts(CanvasDims):
            if self.footer.lower() == 'y':
                 #Footer dims by default
                 self.footer_height = 50
-                self.footer_width = self.width
-                self.footer_horizontal = 0
-                self.footer_vertical = self.height - self.footer_height
+                self.footer_width = self.width - (self.horizontal * 2)
+                self.footer_horizontal = self.horizontal
+                self.footer_vertical = (self.height - self.footer_height) - self.vertical
            else:
                 self.footer_height = 0
                 self.footer_width = 0
@@ -46,9 +46,9 @@ class PredefinedCharts(CanvasDims):
            if self.sidenav.lower() == 'y':
                #sideNav dims by default
                self.side_nav_height = (self.height - self.navbar_height) - self.footer_height
-               self.side_nav_width = 220
-               self.side_nav_horizontal = 0
-               self.side_nav_vertical = self.navbar_height
+               self.side_nav_width = 220 
+               self.side_nav_horizontal = self.horizontal
+               self.side_nav_vertical = (self.navbar_height + self.vertical) + self.vertical 
            else:
                #sideNav dims by default
                self.side_nav_height = 0
@@ -65,23 +65,40 @@ class ChartDims( PredefinedCharts ):
         super().__init__(height , width , horizontal , vertical , navbar , footer , sidenav)
         
         self.total_charts = total_charts
+        #define the space between charts
         self.inner_space = horizontal_space_charts
-        self.outer_space = outside_chart_space
-        self.canvas_new_height = ((self.height - self.navbar_height) - self.footer_height) - (self.outer_space * 2) 
-        self.canvas_new_width = (self.width - self.side_nav_width) - (self.outer_space *2) 
-        self.canvas_new_horizontal = self.horizontal + self.side_nav_width + self.outer_space    
-        self.canvas_new_vertical = (self.vertical + self.navbar_height) + self.outer_space
-             
+        
+        #padding for main canvas
+        self.padding_main_canvas = outside_chart_space
+        
+        
+        self.canvas_new_height = ((self.height - self.navbar_height) - self.footer_height) - (self.padding_main_canvas * 2) 
+        self.canvas_new_width = (self.width - self.side_nav_width) - (self.padding_main_canvas *2) 
+        self.canvas_new_horizontal = self.horizontal + self.side_nav_width + self.padding_main_canvas    
+        self.canvas_new_vertical = (self.vertical + self.navbar_height) + self.padding_main_canvas
+        
+        self.sidenav_new_height = self.side_nav_height - (self.padding_main_canvas * 2)
+        self.sidenav_new_vertical = self.navbar_height + self.padding_main_canvas
+             l
 
 
     def canvas_information(self):
+       
+       table_headers = ["Item |","Height |","Width |","Horizontal |","Vertical |"]
+       table = [ ["Main Canvas:", self.height , self.width , self.horizontal , self.vertical],
+                 ["Navbar:" ,self.navbar_height ,self.navbar_width, self.navbar_horizontal , self.navbar_vertical ],
+                 ["Footer:" ,self.footer_height ,self.footer_width, self.footer_horizontal , self.footer_vertical ],
+                 ["Sidenav:" , self.sidenav_new_height , self.side_nav_width, self.side_nav_horizontal , self.sidenav_new_vertical],
+                 ["Inner Canvas:",self.canvas_new_height, self.canvas_new_width, self.canvas_new_horizontal , self.canvas_new_vertical ]
+                 ] 
+        
        system('cls')
-       print("| Item   | Height   | Width | Vertical | Horizontal |")
-       print("------------------------------------------------------")
-       print(f"| Navbar | {self.navbar_height}       | {self.navbar_width}  | {self.navbar_vertical}        | {self.navbar_horizontal}")
-       print(f"| Footer | {self.footer_height}       | {self.footer_width}  | {self.footer_vertical}     | {self.footer_horizontal}")
-       print(f"| Sidenav| {self.side_nav_height - (self.outer_space * 2 )}     | {self.side_nav_width}     | {self.side_nav_vertical + self.outer_space}    | {self.side_nav_horizontal}")
-       print(f"| Canvas | {self.canvas_new_height}     | {self.canvas_new_width}  | {self.canvas_new_vertical}        | {self.canvas_new_horizontal}")
+       print(tabulate(table , headers= table_headers , showindex="always"))
+       
+       
+       #print(f"{self.footer_height} {self.footer_width} {self.footer_vertical} {self.footer_horizontal}")
+       #print(f"{self.side_nav_height - (self.outer_space * 2 )} {self.side_nav_width} {self.side_nav_vertical + self.outer_space} {self.side_nav_horizontal}")
+       #print(f"{self.canvas_new_height}{self.canvas_new_width}{self.canvas_new_vertical}{self.canvas_new_horizontal}")
 
 
 
